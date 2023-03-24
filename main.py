852b3b3f2c52396ad183eecb99c37fbf30366e22
Victor Lucio <victorlu3391@gmail.com>
2023-03-24 11:47:24 -0300

    Testando modificação - Mudando para LF

--- main.py
+++ main.py
@@ -3,5 +3,4 @@ idade = input()
 ano_futuro = int(input())
 
 idade_nova = (int(ano_futuro) - int(ano_atual)) + int(idade)
-
 print("Fulano, no ano de " + str(ano_futuro) + " você terá " + str(idade_nova) + " anos")
